8a72e01e7e18a061af3ad25115ace32aebfdbcdd
alexYamaoka <ryamaoka1@gmail.com>
2020-10-05 20:28:37 -0700

    created addTodo function

--- app.js
+++ app.js
@@ -4,5 +4,35 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
 // Event Listeners
+todoButton.addEventListener('click', addTodo);
 
 // Functions
+function addTodo(event) {
+    // prevent form from submiting
+    event.preventDefault();
+
+    // Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    // create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+
+    // completed or check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    // trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-check"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+
+}
