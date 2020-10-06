ccfead2ef68567874435a83de89de3f2725d1e30
alexYamaoka <ryamaoka1@gmail.com>
2020-10-05 20:18:31 -0700

    started on js

---
+++ app.js
@@ -0,0 +1,8 @@
+// Selectors 
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+
+// Functions
